9b89d73af3d8f6d81d3243c1998b6bdab2d0676b
Shubh-343 <shubhamstpss@gmail.com>
2023-07-12 07:29:21 +0530

    fix build issue

--- pages/category/[slug].js
+++ pages/category/[slug].js
@@ -12,13 +12,13 @@ const Category = () => {
           <div className="text-[28px] md:text-[34px] font-semibold leading-tight mb-5">Running Shoes</div>
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 px-5 md:px-0 pb-5 my-14">
+          {/* <ProductCard />
           <ProductCard />
           <ProductCard />
           <ProductCard />
           <ProductCard />
           <ProductCard />
-          <ProductCard />
-          <ProductCard />
+          <ProductCard /> */}
         </div>
       </Wrapper>
     </div>
